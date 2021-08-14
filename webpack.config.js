e55410a4644710090a35e11e877dbed5156011b7
vinchinzo123 <vnewsom90@gmail.com>
2021-08-14 01:10:57 -0400

    Sets up react configuration
    For help setting up react from scratch
    https://sasinduusgalhewa.medium.com/how-to-create-a-reactjs-app-from-scratch-22badd7ac8e2

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'public')
+  },
+  module: {
+    rules: [{
+      loader: 'babel-loader',
+      test: /\.js$/,
+      exclude: /node_modules/
+    }]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'public')
+  }
+}
